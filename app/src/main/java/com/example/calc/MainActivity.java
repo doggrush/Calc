2e86acc15e90fd8a794dbb4bfe2dd84a8660e924
augusto_mafra <augusto_mafra@estudante.sesisenai.org.br>
2023-03-23 10:25:42 -0300

    Calculadora

---
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.calc.R;
+
+public class MainActivity extends AppCompatActivity {
+    EditText VI, VF;
+    TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        VI = findViewById(R.id.VFin);
+        VF = findViewById(R.id.VIni);
+        result = findViewById(R.id.result);
+    }
+    public void calculoSoma(View v){
+        int VIn = Integer.parseInt(VI.getText().toString());
+        int VFi = Integer.parseInt(VF.getText().toString());
+        result.setText((VIn+VFi)+"");
+    }
+    public void CalculoSubtracao(View v){
+        int VIn = Integer.parseInt(VI.getText().toString());
+        int VFi = Integer.parseInt(VF.getText().toString());
+        result.setText(""+(VIn - VFi));
+    }
+    public void calculoDivisao(View v){
+        int VIn = Integer.parseInt(VI.getText().toString());
+        int VFi = Integer.parseInt(VF.getText().toString());
+        result.setText((VIn/VFi)+"");
+    }
+    public void calculoMultiplicacao(View v){
+        int VIn = Integer.parseInt(VI.getText().toString());
+        int VFi = Integer.parseInt(VF.getText().toString());
+        result.setText(""+(VIn * VFi));
+    }
+}
